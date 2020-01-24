18d00be66843f9c58b18d0f479a74f572b17c0d7
Alberto Caparrós <acaparros87@gmail.com>
2020-01-24 13:17:27 +0100

    entrega challenges

--- src/playground.ts
+++ src/playground.ts
@@ -1,5 +1,5 @@
 console.log(
-  'This is your playground, experiment with TS code and check the console'
+  'Playground preparado para entrega de challenges modulo Javascript!'
 );
 
 /* 1. Aplanando arrays */
@@ -22,6 +22,7 @@ const sample = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
 let aplanado = arrayMultidimensional => {
   return arrayMultidimensional.reduce(
     (acc, actual) =>
+      //Si es un array, recursión. Si no, concatenamos al array actual que se está reduciendo
       Array.isArray(actual) ? acc.concat(aplanado(actual)) : acc.concat(actual),
     []
   );
@@ -81,6 +82,7 @@ const deepGet = (object, ...properties) => {
   if (properties.length === 0) return object;
   else if (properties.length === 1) return object[properties[0]];
   else {
+    //Mientras haya propiedades vamos accediendo cada vez mas profundo con slice y recursión
     let newProperties = properties.slice(1, properties.length);
     return deepGet(object[properties[0]], ...newProperties);
   }
@@ -106,20 +108,121 @@ comportamiento debería ser:
 
 const myObject2 = {};
 
-const deepSet = (value, object, ...properties) => {
-  if (properties.length === 0) return object;
-  else if (properties.length === 1) return (object[properties[0]] = value);
-  else {
-    let newProperties = properties.slice(1, properties.length);
-    return deepSet(value, object, ...newProperties);
+const deepSet = (value, object, ...properties: Array<string>) => {
+  //Si no hay propiedades, Do nothing
+  if (properties.length > 0) {
+    //Recorremos propiedades
+    while (properties.length - 1) {
+      var property0 = properties.shift(); //Tomamos la propiedad en 0 y la eliminamos de properties, recorriendo el array hacia adelante
+      if (!(property0 in object)) {
+        object[property0] = {}; //Si no existe, asignamos objeto vacio
+      }
+      object = object[property0]; //Asignamos el objeto ya existente, o el nuevo vacio recien creado
+    }
+
+    object[properties[0]] = value; //Asignamos el valor a la ultima propiedad del array que hemos ido reduciendo hacia adelante
   }
 };
 
 deepSet(1, myObject2, 'a', 'b');
-console.log(JSON.stringify(myObject2)); // {a: { b: 1}}
+console.log('CASO 1', JSON.stringify(myObject2)); // {a: { b: 1}}
 deepSet(2, myObject2, 'a', 'c');
-console.log(JSON.stringify(myObject2)); // {a: { b: 1, c: 2}}
+console.log('CASO 2', JSON.stringify(myObject2)); // {a: { b: 1, c: 2}}
 deepSet(3, myObject2, 'a');
-console.log(JSON.stringify(myObject2)); // {a: 3}
+console.log('CASO 3', JSON.stringify(myObject2)); // {a: 3}
 deepSet(4, myObject2);
-console.log(JSON.stringify(myObject2)); // Do nothing // {a: 3}
+console.log('CASO 4', JSON.stringify(myObject2)); // Do nothing // {a: 3}
+
+console.log('----------------------------------------------------------');
+console.log('----------------------------------------------------------');
+console.log('------------------- EJERCICIO 3 - Árbol ------------------');
+console.log('------------------- Respuesta en código! ------------------');
+console.log('----------------------------------------------------------');
+/* ¿Cómo generarías con TypeScript un tipado para estructuras en forma de árbol? Un árbol es una estructura
+que parte de un nodo raiz, a partir del cual salen más nodos. Cada nodo en un árbol puede tener hijos (más
+nodos) o no tenerlos (convirtiendose en un nodo final o una "hoja"). */
+
+interface Arbol<T> {
+  value: T;
+  node: Array<Arbol<T>>;
+}
+
+console.log('----------------------------------------------------------');
+console.log('----------- EJERCICIO 4 - Trazas por consola -------------');
+console.log('----------------------------------------------------------');
+/* Las trazas resultante en consola son:
+first;
+second;
+third;
+El ejercicio consiste en reordenar las trazas para que se muestren invertidas, es decir, con el siguiente orden:
+third;
+second;
+first;
+Pero para ello tan solo podrás modificar la función run .
+Queda prohibido modificar cualquier otro código asi como el contenido de triggers . */
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const showMessage = async ([time, message]) => {
+  await delay(time);
+  console.log(message);
+};
+const triggers = [
+  async () => await showMessage([200, 'third']),
+  async () => await showMessage([100, 'second'])
+];
+const run = async (triggers: Array<any>) => {
+  for (let trigger of triggers) {
+    await trigger();
+  }
+  console.log('first');
+};
+
+//Está puesto así para que quede bien el log de consola
+run(triggers).then(() => {
+  console.log('----------------------------------------------------------');
+  console.log('------------------ EJERCICIO 5 - Memoization -------------');
+  console.log('----------------------------------------------------------');
+  console.log('----------------------------------------------------------');
+  console.log('--------------------- APARTADO A y B ----------------------');
+  console.log('----------------------------------------------------------');
+  /* 
+Implementa un mecanismo de memoización para funciones costosas y tipalo con TypeScript. La memoización
+optimiza sucesivas llamadas del siguiente modo: */
+  const expensiveFunction = () => {
+    console.log('Una única llamada');
+    return 3.1415;
+  };
+
+  let x = expensiveFunction();
+  const memoize = f => (x ? x : f());
+
+  const memoized = () => memoize(expensiveFunction);
+  console.log(memoized()); // Una única llamada // 3.1415
+  console.log(memoized()); // 3.1415
+  console.log(memoized()); // 3.1415
+
+  console.log('----------------------------------------------------------');
+  console.log('--------------------- APARTADO C -------------------------');
+  console.log('----------------------------------------------------------');
+  /* Contempla ahora la posibilidad de que la función a memoizar pueda tener argumentos. Por simplicidad
+supongamos sólo argumentos primitivos: string , number o boolean y que no sean undefined . ¿Podrías
+hacer una versión aceptando argumentos? ¿Cómo la tiparías con TS? Un ejemplo de comportamiento podría
+ser: */
+
+  let count = 0; // Comprobacion de nº de ejecuciones
+  const repeatText = (repetitions: number, text: string): string => (
+    count++, `${text} `.repeat(repetitions).trim()
+  );
+
+  const memoize2 = (f: Function) => {
+    console.log(f.arguments);
+    return f();
+  };
+
+  const memoizedGreet = memoize2(repeatText);
+  console.log(memoizedGreet(1, 'pam')); // pam
+  console.log(memoizedGreet(3, 'chun')); // chun chun chun
+  console.log(memoizedGreet(1, 'pam')); // pam
+  console.log(memoizedGreet(3, 'chun')); // chun chun chun
+  console.log(count); // 2
+});
